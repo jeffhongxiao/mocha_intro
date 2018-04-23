003db6e393bed9a1b55e4e49cae6afb4b07a582d
hongxiao <hongtao.xiao@unb.ca>
2018-04-23 14:11:49 -0300

    initial commit of retry.js (copied from document)

---
+++ test/retry.js
@@ -0,0 +1,21 @@
+
+process.env.NODE_ENV = 'test';
+
+var chai = require('chai');
+var should = chai.should();
+
+// TODO needs setup of Selenium
+describe('retries', function() {
+  // Retry all tests in this suite up to 4 times
+  this.retries(4);
+
+  beforeEach(function () {
+    browser.get('http://www.yahoo.com');
+  });
+
+  it('should succeed on the 3rd try', function () {
+    // Specify this test to only retry up to 2 times
+    this.retries(2);
+    expect($('.foo').isDisplayed()).to.eventually.be.true;
+  });
+});
