3eb7ca2c1e55773815a708a9ea8ed1b8f731b090
hongxiao <hongtao.xiao@unb.ca>
2018-04-25 15:09:43 -0300

    use chai.should() for assertion; remove unused library

--- browser/spec.js
+++ browser/spec.js
@@ -1,18 +1,18 @@
-// var chai = require('chai');
 var should = chai.should();
+
+mocha.setup('bdd');
 
 describe('suite', function() {
   describe('test', function() {
     it('should return 2 + 2 is 4', function(done) {
       var result = 2 + 2;
-      // result.should.equal(4);
-      assert(result === 4);
+      result.should.be.a('number');
+      result.should.equal(4);
 
       done();
     });
 
     it('should return true is true', function(done) {
-      assert(true === true);
       true.should.equal(true);
       done();
     });
@@ -21,13 +21,13 @@ describe('suite', function() {
 
 describe('another suite', function() {
   it('synchronous tests - no callback', function() {
-    assert(true === true);
+    true.should.equal(true);
   });
 });
 
 describe('yet another suite', function() {
   it('asynchronous tests - calls with done', function(done) {
-    assert(true === true);
+    true.should.equal(true);
     done();
   });
 });
